16e0123216ae611562c503bff6c6a4e60c453a1d
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2025-08-15 23:14:27 +0300

    innitial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap()],
+
+  prefetch: {
+    prefetchAll: true,
+  },
+
+  image: {
+    layout: "constrained",
+    responsiveStyles: true,
+  },
+
+  vite: {
+    plugins: []
+  }
+});
